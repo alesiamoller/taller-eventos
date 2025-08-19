8701cb76fd4690c9c33c74475115d73e77ba42ba
alesiamoller <alemoller10@gmail.com>
2025-08-18 23:27:27 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -1,6 +1,11 @@
-
 const div = document.getElementById("divbuton");
+const boton = document.getElementById("clicbuton");
+
+boton.addEventListener("click", function(event) {
+  event.stopPropagation();
+  alert("Hola!");
+});
 
 div.addEventListener("click", function() {
-  alert("Hola! Soy el div");
+  alert("Hola, soy el div");
 });
